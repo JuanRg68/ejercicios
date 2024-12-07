eab67f4300007ad110d8e98caac59fed77252389
JuanRg68 <juanrugeles97@gmail.com>
2024-12-06 23:48:27 -0500

    feat: update README.md

---
+++ clase3/area triangulo1.py
@@ -0,0 +1,4 @@
+def area (base, altura):
+    return base * altura 
+
+print(f" El area del rectangulo es: {area(15, 8)} ")
---
+++ clase3/ejemplo def.py
@@ -0,0 +1,10 @@
+#def sumar(a, b):
+ #   return a + b
+
+#resultado = sumar(1, -1)
+#print(f"El resultado de la suma es: {resultado}")
+
+def calcular_area_rectangulo(base, altura):
+    return base * altura
+
+print(f"El área es: {calcular_area_rectangulo(5, 3)}")
